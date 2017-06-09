18a4a88c4cba7f8008a22391eb509692fde86864
Octavian Fedorovici <octavianfedorovici@gmail.com>
2017-06-09 22:35:38 +0300

    Create laika.js

--- laika.js
+++ laika.js
@@ -4,14 +4,3 @@ function showRestart() {
     document.getElementById('restart').style.webkitAnimationPlayState = "running";
     document.getElementById('restart').style.animationPlayState = "running";
 }
-
-function previewFile(){
-       var preview = document.querySelector('img'); //selects the query named img
-       var reader  = new FileReader();
-       reader.onloadend = function () {
-           preview.src = reader.result;
-       }
-       preview.src = "";
-  }
-
-  previewFile();
