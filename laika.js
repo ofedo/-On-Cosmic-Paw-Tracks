86633b8675473f71a34b5326435f334afcf5e5e5
Octavian Fedorovici <octavianfedorovici@gmail.com>
2017-06-09 22:29:35 +0300

    Create laika.js

--- laika.js
+++ laika.js
@@ -7,7 +7,7 @@ function showRestart() {
 
 function previewFile(){
        var preview = document.querySelector('img'); //selects the query named img
-       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var file    = false;//document.querySelector('input[type=file]').files[0]; //sames as here
        var reader  = new FileReader();
 
        reader.onloadend = function () {
